83979d151e6d8522e59ba7e1fff96eda22ed5e3d
hollowdoor <hollowdoor99@gmail.com>
2014-12-09 08:07:07 -0600

    HTTP server static send test added.

--- test/server.js
+++ test/server.js
@@ -6,11 +6,11 @@ var http = require('http'),
 
 var server = http.createServer(function(req, res){
     
-    var mashing = dara.send(req, res);
+    var sender = dara.send(req, res);
     
-    console.log('mashing ', mashing);
-    if(!mashing){
-        console.log('no mash');
+    console.log('sender = ', sender);
+    if(!sender.sending){
+        console.log('no send');
         if(path.basename(req.url) === 'http.html'){
             fs.readFile(path.join(__dirname,'/http.html'), {encoding: 'utf-8'}, function(err, text){
                 if(err){
