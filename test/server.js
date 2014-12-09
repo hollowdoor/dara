8cb1e348a8030904f3a17065dc556f2b0a8a170b
hollowdoor <hollowdoor99@gmail.com>
2014-12-09 07:48:34 -0600

    Added author to the package

--- test/server.js
+++ test/server.js
@@ -1,18 +1,18 @@
 var http = require('http'),
-    mash = require('../mash'),
+    dara = require('../dara'),
     fs = require('fs'),
     path = require('path');
 
 
 var server = http.createServer(function(req, res){
     
-    var mashing = mash.send(req, res);
+    var mashing = dara.send(req, res);
     
     console.log('mashing ', mashing);
     if(!mashing){
         console.log('no mash');
-        if(path.basename(req.url) === 'test.html'){
-            fs.readFile(path.join(__dirname,'/test.html'), {encoding: 'utf-8'}, function(err, text){
+        if(path.basename(req.url) === 'http.html'){
+            fs.readFile(path.join(__dirname,'/http.html'), {encoding: 'utf-8'}, function(err, text){
                 if(err){
                     res.end(err.message);
                     return;
@@ -20,6 +20,8 @@ var server = http.createServer(function(req, res){
                 
                 res.end(text);
             });
+        }else{
+            res.end('Not a valid address. Use localhost:8080/http.html');
         }
     }
 });
